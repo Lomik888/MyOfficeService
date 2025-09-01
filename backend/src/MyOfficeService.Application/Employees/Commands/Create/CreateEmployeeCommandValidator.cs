6dc4628cbe378a1ffec5c7b1d651b87702815f93
Solovev <solovtev17@bk.ru>
2025-09-01 12:53:13 +0300

    фича по созданию сотрудника

---
+++ backend/src/MyOfficeService.Application/Employees/Commands/Create/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MyOfficeService.Application.Extations;
+using MyOfficeService.Domain.Entities;
+using MyOfficeService.Domain.ValueObjects;
+
+namespace MyOfficeService.Application.Employees.Commands.Create;
+
+public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+{
+    public CreateEmployeeCommandValidator()
+    {
+        RuleFor(x => x)
+            .Must(x => Employee.Validate(
+                x.Name,
+                x.CompanyId,
+                x.DepartmentId));
+
+        RuleFor(x => x.Phone).Must(x => Phone.Validate(x));
+
+        RuleFor(x => x)
+            .Must(x => Passport.Validate(x.PassportType, x.PassportNumber));
+    }
+}
