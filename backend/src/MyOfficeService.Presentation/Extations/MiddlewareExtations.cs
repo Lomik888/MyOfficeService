7ab7703ff0c37dee96b94963a0f9122ef74bf833
Solovev <solovtev17@bk.ru>
2025-09-01 12:57:59 +0300

    Глобальный обработчик исключений и мигратор

---
+++ backend/src/MyOfficeService.Presentation/Extations/MiddlewareExtations.cs
@@ -0,0 +1,11 @@
+﻿using MyOfficeService.Presentation.Middlewares;
+
+namespace MyOfficeService.Presentation.Extations;
+
+public static class MiddlewareExtations
+{
+    public static IApplicationBuilder UseGlobalExceptionsHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
---
+++ backend/src/MyOfficeService.Presentation/Services/DatabaseInitializerHostedService.cs
@@ -0,0 +1,27 @@
+﻿using MyOfficeService.Infrastructure.Postgres.Migrations;
+
+namespace MyOfficeService.Presentation.Services;
+
+/// <summary>
+/// DatabaseInitializerHostedService - запускается 1 раз при старте приложения, нужен для того,
+/// чтобы dbup добавил миграции.
+/// </summary>
+public class DatabaseInitializerHostedService : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public DatabaseInitializerHostedService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
+        dbInitializer.Initialize();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
