def16f144577b2db67b28994afc07295b42784f4
Solovev <solovtev17@bk.ru>
2025-09-01 12:50:25 +0300

    Репозиторий для компаний

---
+++ backend/src/MyOfficeService.Application/Repositories/ICompanyRepository.cs
@@ -0,0 +1,28 @@
+﻿using MyOfficeService.Application.Dtos;
+
+namespace MyOfficeService.Application.Repositories;
+
+public interface ICompanyRepository
+{
+    /// <summary>
+    /// GetEmployeesAsync - возвращает всех сотрудников компании без пагинации
+    /// </summary>
+    /// <param name="companyId">Id компании</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Вернёт коллекцию EmployeeDto</returns>
+    Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(
+        int companyId,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// GetEmployeesAsync - возвращает всех сотрудников департамента компании без пагинации
+    /// </summary>
+    /// <param name="companyId">Id компании</param>
+    /// <param name="departmentId">Id департамента компании</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Вернёт коллекцию EmployeeDto</returns>
+    Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(
+        int companyId,
+        int departmentId,
+        CancellationToken cancellationToken = default);
+}
