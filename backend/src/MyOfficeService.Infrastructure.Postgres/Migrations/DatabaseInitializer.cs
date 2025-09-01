894604af6529d2bdef60dc5aec490efc490317b1
Solovev <solovtev17@bk.ru>
2025-09-01 12:47:02 +0300

    Добавил класс для миграций и сами миграции

---
+++ backend/src/MyOfficeService.Infrastructure.Postgres/Migrations/DatabaseInitializer.cs
@@ -0,0 +1,42 @@
+﻿using DbUp;
+
+namespace MyOfficeService.Infrastructure.Postgres.Migrations;
+
+public class DatabaseInitializer
+{
+    private readonly string _connectionString;
+
+    public DatabaseInitializer(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void Initialize()
+    {
+        EnsureDatabase.For.PostgresqlDatabase(_connectionString);
+
+        var upgrader = DeployChanges.To
+            .PostgresqlDatabase(_connectionString)
+            .WithScriptsEmbeddedInAssembly(typeof(DatabaseInitializer).Assembly)
+            .LogToConsole()
+            .Build();
+
+        if (upgrader.IsUpgradeRequired())
+        {
+            var result = upgrader.PerformUpgrade();
+
+            if (result.Successful == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success!");
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ backend/src/MyOfficeService.Infrastructure.Postgres/Migrations/Scripts/0001-Create-Company-Table.sql
@@ -0,0 +1,6 @@
+﻿create table if not exists companies
+(
+    id    serial primary key,
+    name  varchar(150) not null,
+    phone varchar(12)  not null unique
+);
