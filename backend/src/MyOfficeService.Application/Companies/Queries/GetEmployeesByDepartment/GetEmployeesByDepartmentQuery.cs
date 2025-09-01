a52e5c13b60d03fef244ad75f85371623dd54b7e
Solovev <solovtev17@bk.ru>
2025-09-01 12:52:09 +0300

    Фича получение сотрудников департамента компании по Id компании и Id департамента

---
+++ backend/src/MyOfficeService.Application/Companies/Queries/GetEmployeesByDepartment/GetEmployeesByDepartmentQuery.cs
@@ -0,0 +1,5 @@
+﻿using MyOfficeService.Application.Abstractions.Handlers;
+
+namespace MyOfficeService.Application.Companies.Queries.GetEmployeesByDepartment;
+
+public record GetEmployeesByDepartmentQuery(int CompanyId, int DepartmentId) : IQuery;
