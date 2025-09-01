24da152cda18ef4e7e84c03e59edfd562662f333
Solovev <solovtev17@bk.ru>
2025-09-01 12:49:36 +0300

    Интерфейсы для команд и запросов

---
+++ backend/src/MyOfficeService.Application/Abstractions/Handlers/IQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFunctionalExtensions;
+using MyOfficeService.Domain.Errors;
+
+namespace MyOfficeService.Application.Abstractions.Handlers;
+
+/// <summary>
+/// IQueryHandler - для имплементации в Handler.
+/// Является контрактом на асинхронный метод возвращающий Result TResult, TError
+/// </summary>
+/// <typeparam name="TResultValue">Что возращает Handler</typeparam>
+/// <typeparam name="TResultError">Какой тип ошибки при неудачном вызову</typeparam>
+/// <typeparam name="TQuery">Запрос, для которой был реализован Handler</typeparam>
+public interface IQueryHandler<TResultValue, TResultError, in TQuery>
+    where TQuery : IQuery
+    where TResultError : ErrorList
+{
+    public Task<Result<TResultValue, TResultError>> HandleAsync(
+        TQuery request,
+        CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// IQueryHandler - для имплементации в Handler.
+/// Является контрактом на асинхронный метод возвращающий Result TResultError
+/// </summary>
+/// <typeparam name="TResultError">Какой тип ошибки при неудачном вызову</typeparam>
+/// <typeparam name="TQuery">Запрос, для которой был реализован Handler</typeparam>
+public interface IQueryHandler<TResultError, in TQuery>
+    where TQuery : IQuery
+    where TResultError : ErrorList
+{
+    public Task<UnitResult<TResultError>> HandleAsync(
+        TQuery request,
+        CancellationToken cancellationToken = default);
+}
