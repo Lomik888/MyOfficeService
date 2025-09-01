80cef2ae4e9552eaa2bdf332ab7fe7d5fa031543
Solovev <solovtev17@bk.ru>
2025-09-01 12:45:03 +0300

    Добавил классы, для работы с ошибками

---
+++ backend/src/MyOfficeService.Domain/Enums/ErrorCodes.cs
@@ -0,0 +1,33 @@
+﻿namespace MyOfficeService.Domain.Enums;
+
+/// <summary>
+/// ErrorCodes — общий каталог предопределённых кодов ошибок (фабрика кодов ошибок)
+/// </summary>
+public static class ErrorCodes
+{
+    /// <summary>
+    /// Коды ошибок для валидации
+    /// </summary>
+    public static class Validation
+    {
+        public const string INVALID_FIELD = "invalid.field";
+    }
+
+    /// <summary>
+    /// Коды ошибок для сервера
+    /// </summary>
+    public static class Server
+    {
+        public const string SERVER_ERROR = "server.error";
+    }
+
+    /// <summary>
+    /// Коды ошибок для БД
+    /// </summary>
+    public static class Database
+    {
+        public const string NOT_FOUND = "not.found";
+        public const string CAN_NOT_CREATE = "can.not.create";
+        public const string TRANSACTION_ERROR = "transaction.error";
+    }
+}
---
+++ backend/src/MyOfficeService.Domain/Enums/ErrorTypes.cs
@@ -0,0 +1,22 @@
+﻿namespace MyOfficeService.Domain.Enums;
+
+/// <summary>
+/// ErrorTypes - типы ошибок
+/// </summary>
+public enum ErrorTypes
+{
+    /// <summary>
+    /// Ошибка валидации
+    /// </summary>
+    VALIDATION,
+
+    /// <summary>
+    /// Ошибка БД
+    /// </summary>
+    DB,
+
+    /// <summary>
+    /// Ошибка сервера
+    /// </summary>
+    SERVER
+}
